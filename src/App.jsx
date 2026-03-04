afd30339829ea52dd74dadcc7ba996310cb780e2
ianchan3 <ianchan3138@gmail.com>
2026-03-03 16:19:02 -0800

    React Router Dom Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import React from "react";
-import Navigate from "react-router-dom"
+import { Navigate } from "react-router-dom"
 import HomePage from "./HomePage/HomePage";
 import MenuPage from "./MenuPage/MenuPage";
 import SuccessPage from "./SuccessPage/SuccessPage";

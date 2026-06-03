d52b7b2bb93fd25c86c5307f014c59094cfa71b0
ianchan3 <ianchan3138@gmail.com>
2026-06-02 17:23:01 -0700

    Config Changes

--- server/app.js
+++ server/app.js
@@ -31,11 +31,16 @@
     })
   );
 
-  app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-  }))
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    secure: process.env.NODE_ENV === 'production' ? true : false,
+    maxAge: 24 * 60 * 60 * 1000
+  }
+}))
   app.use(passport.initialize())
   app.use(passport.session())
 

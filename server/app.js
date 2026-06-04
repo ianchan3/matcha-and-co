4f7bad48b1988d4ee7a4abc67f598b647b391cb2
ianchan3 <ianchan3138@gmail.com>
2026-06-03 21:59:58 -0700

    Revert "Memory Leak Fix"
    This reverts commit 50d685fbd2356c285ada557ebf77156b673a0623.

--- server/app.js
+++ server/app.js
@@ -5,7 +5,6 @@
   import checkoutRoutes from './routes/checkout.js';
   import contactRoutes from './routes/contact.js';
   import session from 'express-session';
-  import MongoStore from 'connect-mongo';
   import authRoutes from './routes/auth.js';
   import passport from 'passport';
   import './config/passport.js'; 
@@ -37,9 +36,6 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
-  store: MongoStore.create({
-    mongoUrl: process.env.MONGODB_URI
-  }),
   cookie: {
     sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     secure: process.env.NODE_ENV === 'production' ? true : false,

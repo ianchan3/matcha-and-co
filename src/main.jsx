18852a67b2738d9b3b21ea37abc83026fd21ef2f
ianchan3 <ianchan3138@gmail.com>
2026-07-02 18:38:49 -0700

    Revert Linting Fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import i18n from './i18n/index.js';
 import React from "react";
 import './index.css'
 import App from './App.jsx'
